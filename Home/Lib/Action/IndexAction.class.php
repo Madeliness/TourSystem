d2646d2ca0301e986e243b012a352d7b26e5f9e7
zhang__jiaojiao <445156830@qq.com>
2017-05-16 17:12:16 +0800

    动态添加热门酒店信息

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -6,13 +6,16 @@ class IndexAction extends Action {
 		$t=M('Tourist');  //$m模型的实例
 		$tidmax=$t->max('tid');
 		$where['tid']=array('elt',$tidmax);
-		$arr = $t->where($where)->limit(8)->order('tid desc')->getField('tid,tname,tcity,ticket');
+		$arr1 = $t->where($where)->limit(8)->order('tid desc')->getField('tid,tname,tcity,ticket');
 		//var_dump($arr);
-		$this->assign('data',$arr);
+		$this->assign('tour',$arr1);
 		
-	
-	
-	
+		//展示4个热门酒店
+		$h=M('Hotel');
+		$hidmax=$h-max('hid');
+		$data['hid']=array('elt',$hidmax);
+		$arr2 = $h->where($data)->limit(4)->order('hid desc')->getField('hid,hname,hcity,hlevel,hphone');
+		$this->assign('hotel',$arr2);
 	
 	
 	
