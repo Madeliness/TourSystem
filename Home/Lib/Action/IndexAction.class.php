867534fcc43edc25cfb52554b2019413883e5d27
zhang__jiaojiao <445156830@qq.com>
2017-05-16 18:52:32 +0800

    风景二级页面完善1

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -5,19 +5,19 @@ class IndexAction extends Action {
 		//展示8个热门景点
 		$t=M('Tourist');  //$m模型的实例
 		$tidmax=$t->max('tid');
-		$where['tid']=array('elt',$tidmax);
-		$arr1 = $t->where($where)->limit(8)->order('tid desc')->getField('tid,tname,tcity,ticket');
+		$where1['tid']=array('elt',$tidmax);
+		$arr1 = $t->where($where1)->order('tid desc')->limit(8)->getField('tid,tname,tcity,ticket');
 		//var_dump($arr);
 		$this->assign('tour',$arr1);
 		
 		//展示4个热门酒店
 		$h=M('Hotel');
-		$hidmax=$h-max('hid');
-		$data['hid']=array('elt',$hidmax);
-		$arr2 = $h->where($data)->limit(4)->order('hid desc')->getField('hid,hname,hcity,hlevel,hphone');
+		$hidmax=$h->max('hid');
+		$where2['hid']=array('elt',$hidmax);
+		$arr2 = $h->where($where2)->limit(4)->order('hid desc')->getField('hid,hname,hcity,hlevel,hphone');
 		$this->assign('hotel',$arr2);
-	
-	
+		
+		//展示4个热门路线
 	
 	
 	
--- Home/Lib/Action/TourAction.class.php
+++ Home/Lib/Action/TourAction.class.php
@@ -1,8 +1,15 @@
 <?php
 	class TourAction extends Action{
 		public function index(){
-			$t=M('Tourist');
-			
+			$t=M('Tourist');	
+			import('ORG.Util.Page');
+			$count      = $t->count();// 查询满足要求的总记录数
+			$Page       = new Page($count,9);// 实例化分页类 传入总记录数和每页显示的记录数
+			$show       = $Page->show();// 分页显示输出
+			// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+			$list = $t->field('tid,tname,tcity,ticket,tabstract')->limit($Page->firstRow.','.$Page->listRows)->select();
+			$this->assign('list',$list);// 赋值数据集
+			$this->assign('page',$show);// 赋值分页输出
 			$this->display();
 			
 		}
